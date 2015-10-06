fe49da2fc4da452b1beb60540b50cf3eb09fb729
Evan <evan@EvandeiMac.local>
2015-10-06 17:33:27 +0800

    first commit

---
+++ CellViewController.h
@@ -0,0 +1,13 @@
+//
+//  CellViewController.h
+//  UICollectionViewInsideOfCollectionView
+//
+//  Created by Evan on 2015/10/6.
+//  Copyright (c) 2015年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellViewController : UIViewController
+
+@end
---
+++ UICollectionViewInsideOfCollectionView/NumberCell.m
@@ -0,0 +1,13 @@
+//
+//  NumberCell.m
+//  UICollectionViewInsideOfCollectionView
+//
+//  Created by Evan on 2015/10/6.
+//  Copyright (c) 2015年 Evan. All rights reserved.
+//
+
+#import "NumberCell.h"
+
+@implementation NumberCell
+
+@end
